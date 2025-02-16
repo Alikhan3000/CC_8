f82ffa778aa65e6fa2d7adf317d6888ef314b26c
Alikhan <fixator2004@gmail.com>
2025-02-16 17:39:28 -0500

    Task 3 - Service Fee Calculation.

--- cc_8.js
+++ cc_8.js
@@ -20,3 +20,23 @@ let calculateDiscount = function(price, discountRate){      //declared a functio
 
 console.log(`Final Price: $${calculateDiscount(100, 0.2)}`); //logged the output using test data and template literal // Expected output: "Final Price: $80.00"
 console.log(`Final Price: $${calculateDiscount(250, 0.15)}`); // Expected output: "Final Price: $212.50"
+
+//Task 3: Arrow Function
+
+let calculateServiceFee = (amount, serviceType) => {        //declared an arrow function with 2 variables
+    let fee = 0;                                            //declared a variable to store the function output
+
+    if (serviceType === "Premium") {                        //used if else statement to assign the right fee percentage based on the category; 
+        fee = 0.15 * amount;                                //once the category is known fee percentage is multiplied by the amount to get the final price
+        
+    } else if (serviceType === "Standard"){
+        fee = 0.10 * amount;
+
+    }else{fee = 0.05 * amount};
+
+    return fee.toFixed(2);                                  //returned function output; rounded to 2 decimals
+};
+
+console.log(`Service Fee: $${calculateServiceFee(200, "Premium")}`); //logged the function using test data // Expected output: "Service Fee: $30.00"
+console.log(`Service Fee: $${calculateServiceFee(500, "Standard")}`); // Expected output: "Service Fee: $50.00"
+
