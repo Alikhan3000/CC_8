f24fad32e0d5860d2897bbd6f054c79c665d7ee1
Alikhan <fixator2004@gmail.com>
2025-02-16 17:22:21 -0500

    Task 2 - Product Price After Discount.

--- cc_8.js
+++ cc_8.js
@@ -9,3 +9,14 @@ function calculateSalary(baseSalary, bonus, taxRate) {              //declared a
 
 console.log(`Net Salary: $${calculateSalary(5000, 500, 0.1)}`); //logged the output using test data and template literal // Expected output: "Net Salary: $5000.00"
 console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15)}`); // Expected output: "Net Salary: $6950.00"
+
+//Task 2: Function Expression
+
+let calculateDiscount = function(price, discountRate){      //declared a function expression with 2 variables
+    let finalPrice = price - (price * discountRate)         //utilized the formula to calculate final price
+    return finalPrice.toFixed(2);                           //returned function output
+
+};
+
+console.log(`Final Price: $${calculateDiscount(100, 0.2)}`); //logged the output using test data and template literal // Expected output: "Final Price: $80.00"
+console.log(`Final Price: $${calculateDiscount(250, 0.15)}`); // Expected output: "Final Price: $212.50"
